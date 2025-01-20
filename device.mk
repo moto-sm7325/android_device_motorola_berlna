d7a7fd4e162fcc4d71de0172d59f66c7f396fd9a
Nolen Johnson <johnsonnolen@gmail.com>
2025-01-20 01:01:51 -0500

    berlna: Define a soong namespace
    Change-Id: Ie9baab9b7691bb568ba61c2f703aec1661f328c1

--- device.mk
+++ device.mk
@@ -109,5 +109,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/berlna/berlna-vendor.mk)
